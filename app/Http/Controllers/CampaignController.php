a18e878b1394dee10818b87fe3732e1323fec901
Marcos Cantarutti <marcossandim21@gmail.com>
2025-01-29 20:17:53 -0300

    adicionado uso de session

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -31,32 +31,99 @@ class CampaignController extends Controller
 
     public function create(?string $tab = null)
     {
+        // session()->forget('campaigns::create');
+
         return view('campaigns.create', [
             'tab' => $tab,
             'form' => match ($tab) {
                 'template' => '_template',
                 'schedule' => '_schedule',
                 default => '_config'
-            }
+            },
+
+            'data' => session()->get('campaigns::create', [
+                'name' => null,
+                'subject' => null,
+                'email_list_id' => null,
+                'template_id' => null,
+                'body' => null,
+                'track_click' => null,
+                'track_open' => null,
+                'sent_at' => null,
+            ])
         ]);
     }
 
     public function store(?string $tab = null)
     {
-        if (blank($tab)) { // esta vindo do create =  aba 1°
+        $toRoute = '';
 
-            $data = request()->validate([
+        $map = array_merge([
+            'name' => null,
+            'subject' => null,
+            'email_list_id' => null,
+            'template_id' => null,
+            'body' => null,
+            'track_click' => null,
+            'track_open' => null,
+            'sent_at' => null,
+        ], request()->all());
+
+
+
+        if (blank($tab)) {
+            request()->validate([
                 'name' => ['required', 'max:255'],
                 'subject' => ['required', 'max:40'],
                 'email_list_id' => ['nullable'],
                 'template_id' => ['nullable'],
+                'track_click' => ['nullable'],
+                'track_open' => ['nullable'],
+                'sent_at' => ['nullable'],
             ]);
 
-            session()->put('campaigns::create', $data);
-
-            return to_route('campaigns.create', ['tab' => 'template']);
+            $toRoute = route('campaigns.create', ['tab' => 'template']);
         }
+        // dd($tab);
+        if ($tab == 'template') {
+            request()->validate(['body' => ['required']]);
+            $toRoute = route('campaigns.create', ['tab' => 'schedule']);
+        }
+
+        if ($tab == 'schedule') {
+
+            request()->validate(['sent_at' => ['required', 'date']]);
+            // $toRoute = route('campaigns.index');
+        }
+
+        // Garante que a sessão tem todas as chaves
+        $session = session('campaigns::create', []);
+        $defaults = [
+            'name' => null,
+            'subject' => null,
+            'email_list_id' => null,
+            'template_id' => null,
+            'body' => null,
+            'track_click' => null,
+            'track_open' => null,
+            'sent_at' => null,
+        ];
+
+        // Preenche chaves ausentes na sessão
+        $session = array_merge($defaults, $session);
+
+        foreach ($session as $key => $value) {
+            $newValue = data_get($map, $key);
+            if (filled($newValue)) {
+                $session[$key] = $newValue;
+            }
+        }
+
+        session()->put('campaigns::create', $session);
+
+        return redirect($toRoute);
     }
+
 
 
     public function destroy(Campaign $campaign)
--- resources/views/campaigns/create/_template.blade.php
+++ resources/views/campaigns/create/_template.blade.php
@@ -1,5 +1,16 @@
 <div>
     <x-input-label for="body" :value="__('Body')" />
-    <x-input.richtext name="body" :value="old('body')" />
+    <x-input.richtext name="body" :value="old('body', $data['body'])" />
     <x-input-error :messages="$errors->get('body')" class="mt-2" />
+</div>
+
+
+<div class="flex items-center space-x-4">
+    <x-button.link secondary :href="route('campaigns.index')">
+        {{__('Cancel')}}
+    </x-button.link>
+
+    <x-button.primary type="submit">
+        {{__('Save')}}
+    </x-button.primary>
 </div>
